a747f565393b6421842c7b062ea770b0cfad2934
ramkasula2019 <ram.kasula@cotiviti.com>
2022-06-01 07:34:47 +0545

    4_reverse_link

--- tutorials/snippets/urls.py
+++ tutorials/snippets/urls.py
@@ -3,10 +3,25 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    path('', views.api_root),
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),    
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+    path('users/',
+        views.UserList.as_view(),
+        name='user-list'),
+    ## how to make this user-detail works in reverse
+    # path('users/<int:pk>/',
+    #     views.UserDetail.as_view(),
+    #     name='user-detail'),
+    path('snippets/',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    # path('snippets/<int:pk>/',
+    #     views.SnippetDetail.as_view(),
+    #     name='snippet-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
