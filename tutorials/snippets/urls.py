1c084e4719b054542a5d128de202438a23dfeaf9
ramkasula2019 <ram.kasula@cotiviti.com>
2022-06-01 13:36:28 +0545

    6_b4_using_routers

--- tutorials/snippets/urls.py
+++ tutorials/snippets/urls.py
@@ -1,27 +1,62 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import views
+# from snippets import views
 
-urlpatterns = [
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(),name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(),name='snippet-detail'),
-    # path('users/', views.UserList.as_view()),
-    # path('users/<int:pk>/', views.UserDetail.as_view()),    
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name ='snippet-highlight'),
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-    ## how to make this user-detail works in reverse
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail'),
-    # path('snippets/',
-    #     views.SnippetList.as_view(),
-    #     name='snippet-list'),
-    # # path('snippets/<int:pk>/',
-    #     views.SnippetDetail.as_view(),
-    #     name='snippet-detail')
-]
+# urlpatterns = [
+#     path('', views.api_root),
+#     path('snippets/', views.SnippetList.as_view(),name='snippet-list'),
+#     path('snippets/<int:pk>/', views.SnippetDetail.as_view(),name='snippet-detail'),
+#     # path('users/', views.UserList.as_view()),
+#     # path('users/<int:pk>/', views.UserDetail.as_view()),    
+#     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name ='snippet-highlight'),
+#     path('users/',
+#         views.UserList.as_view(),
+#         name='user-list'),
+#     ## how to make this user-detail works in reverse
+#     path('users/<int:pk>/',
+#         views.UserDetail.as_view(),
+#         name='user-detail'),
+#     # path('snippets/',
+#     #     views.SnippetList.as_view(),
+#     #     name='snippet-list'),
+#     # # path('snippets/<int:pk>/',
+#     #     views.SnippetDetail.as_view(),
+#     #     name='snippet-detail')
+# ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+##### the below is done for 6-viewsets routers topic tutorial
+
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+
+urlpatterns = format_suffix_patterns([
+    path('', api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
+])
---
+++ tutorials/snippets/views_7.py
@@ -0,0 +1,56 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+        ### how to make it works when there are details 
+        # 'users_detail': reverse('user-detail', request=request, format=format),
+        # 'snippets_detail': reverse('snippet-detail', request=request, format=format)
+    })
+
+class SnippetList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
